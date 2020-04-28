573d2675228a67fa703be408be257f2a4a45dd61
Sylwia Fait <sylwia.fait@gmail.com>
2020-04-28 22:01:34 +0200

    Update menu (hide)

--- js/app.js
+++ js/app.js
@@ -20,11 +20,19 @@ function changeTitle() {
 function showHamburgerMenu() {
     const hamburger = document.querySelector(".hamburger");
     const nav = document.querySelector(".navigation");
+    const navigationLinks = document.querySelectorAll(".navigation__link");
 
     const handleClick = function() {
         hamburger.classList.toggle("hamburger--active");
         hamburger.setAttribute("aria-expanded", hamburger.classList.contains("hamburger--active"));
         nav.classList.toggle("navigation--active");
+    }
+
+    for (let i = 0; i < navigationLinks.length; i++) {
+        navigationLinks[i].addEventListener("click", function() {
+            hamburger.classList.remove("hamburger--active");
+            nav.classList.remove("navigation--active");
+        })
     }
 
     hamburger.addEventListener("click", handleClick);
